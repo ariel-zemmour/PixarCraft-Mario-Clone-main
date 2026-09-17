ea00ffce514e2628c72dbf892b3f9d594fec8b7d
Ariel <ariel@example.com>
2026-09-17 15:17:46 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/PixarCraft-Mario-Clone-main/sw.js', { scope: '/PixarCraft-Mario-Clone-main/' })})}
