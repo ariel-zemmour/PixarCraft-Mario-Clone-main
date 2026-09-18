b335ff5a7322e33596879f575a5493571ac37b9d
Ariel <ariel@example.com>
2026-09-18 13:35:31 +0300

    Reduce airplane blue coin spawn rate

--- PixarCraft-Mario-Clone-main/src/pages/Play.tsx
+++ PixarCraft-Mario-Clone-main/src/pages/Play.tsx
@@ -655,7 +655,7 @@ export default function App() {
         spikesRef.current.push({ x: currentX + platW/2 - 50, y: platY - 20, w: 100, h: 20 });
       }
       
-      if (nextRandom() > 0.85) {
+      if (nextRandom() > 0.97) {
         blueCoinsRef.current.push({ x: currentX + platW/2, y: platY - 150, w: 20, h: 20, type: 'blueCoin' });
       }
 
